f4f338c6fb25f19d838d50d72c6bae9ba145456d
grateful-library-0184 <alexcouts18@gmail.com>
2019-03-01 04:32:46 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -36,11 +36,11 @@ function prependKitten(name){
 }
 
 function removeLastKitten(){
-  
+  return kittens.slice(1)
 }
 
 function removeFirstKitten(){
-  
+  return kittens.slice(0,kittens.length - 1)
 }
 
 
